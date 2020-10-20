a9131695ffd032bc5d659feda6329a50fc9f3229
KPKM <kirill.pakhomov@lanit-tercom.com>
2020-10-20 17:27:26 +0300

    test(components):
    Jira issue: VEGA-523

--- src/components/Table2/TableCell2/validateValue.test.ts
+++ src/components/Table2/TableCell2/validateValue.test.ts
@@ -4,7 +4,7 @@ describe('validateValue tableCell2', () => {
   test('input in tableCell2', () => {
     const input = '111,,.3.,44.';
     const input2 = '111,,.3.,44.77';
-    const output = '111344.00';
+    const output = '111344';
     const output2 = '111344.77';
     expect(validateValue(input)).toEqual(output);
     expect(validateValue(input2)).toEqual(output2);
