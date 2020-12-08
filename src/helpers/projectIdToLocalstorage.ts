8bf6bb9753db3d9f6a8a9dc80aed294604a2d5e3
ababakov <ababakov@inbox.ru>
2020-12-08 12:22:41 +0700

    fix: set projectId from url
    Jira issue: VEGA-2787

--- src/helpers/projectIdToLocalstorage.ts
+++ src/helpers/projectIdToLocalstorage.ts
@@ -1,11 +1,9 @@
 const localStorageKey = 'projectId';
 
 export function projectIdFromLocalStorage() {
-  let value = sessionStorage.getItem(localStorageKey) || '';
-  if (!value) {
-    sessionStorage.setItem(localStorageKey, 'a3333333-b111-c111-d111-e00000000000');
-    value = sessionStorage.getItem(localStorageKey) || '';
-  }
+  return sessionStorage.getItem(localStorageKey) || '';
+}
 
-  return value;
+export function setProjectId(id: string) {
+  sessionStorage.setItem(localStorageKey, id);
 }
