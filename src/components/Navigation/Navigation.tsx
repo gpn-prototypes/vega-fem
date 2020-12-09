ffe474ff15d54836c805d36f934004cceee370fa
ababakov <ababakov@inbox.ru>
2020-12-09 19:32:40 +0700

    feat: navigation disabled items
    Jira issue: VEGA-2863

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,16 +1,21 @@
-import React, { useContext, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { ChoiceGroup } from '@gpn-prototypes/vega-ui';
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Badge, Text } from '@gpn-prototypes/vega-ui';
+import bemCn from 'bem-cn';
+
+import './Navigation.css';
 
 import { ProjectContext } from '@/providers';
+
+export const cnNavigation = bemCn('Navigation');
 
 export interface NavItem {
   title: string;
   path: string;
+  disabled?: boolean;
 }
 
 export const Navigation: React.FC = () => {
-  const history = useHistory();
   const { projectId } = useContext(ProjectContext);
 
   const prefix = `/projects/show/${projectId}/fem`;
@@ -23,10 +28,12 @@ export const Navigation: React.FC = () => {
     {
       title: 'Налоговое окружение',
       path: `${prefix}/tax-environment`,
+      disabled: true,
     },
     {
       title: 'Цены',
       path: `${prefix}/prices`,
+      disabled: true,
     },
     {
       title: 'OPEX',
@@ -39,27 +46,42 @@ export const Navigation: React.FC = () => {
     {
       title: 'Профиль добычи',
       path: `${prefix}/mining-profile`,
+      disabled: true,
     },
   ];
-  const currentTab = tabs.find((element) => {
-    return element.path === history.location.pathname;
-  }) as NavItem;
-  const [valueTab, setValueTab] = useState<NavItem | null>(currentTab);
 
   return (
-    <ChoiceGroup
-      size="s"
-      view="primary"
-      form="default"
-      name=""
-      multiple={false}
-      items={tabs}
-      value={valueTab}
-      getLabel={(item): string => item.title}
-      onChange={({ value }): void => {
-        setValueTab(value);
-        history.push(value ? value.path : '/');
-      }}
-    />
+    <div
+      className={cnNavigation({ size: 's', form: 'default', view: 'primary', width: 'default' })}
+    >
+      {tabs.map(({ title, path, disabled }) => {
+        if (disabled) {
+          return (
+            <div className={cnNavigation('Label', { disabled: true })}>
+              <Text view="ghost">{title}</Text>
+              <Badge
+                className={cnNavigation('Badge').toString()}
+                label="Скоро"
+                view="filled"
+                status="system"
+                size="s"
+                form="round"
+              />
+            </div>
+          );
+        }
+
+        return (
+          <NavLink
+            exact
+            activeClassName={cnNavigation('Label', { checked: true })}
+            to={path}
+            className={cnNavigation('Label')}
+          >
+            {title}
+          </NavLink>
+        );
+      })}
+    </div>
   );
 };
