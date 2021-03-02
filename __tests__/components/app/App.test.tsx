0ee3943dbad9b279ba4b683af28587e6deac755d
tumoyakov <leonid.tumoyakov@gmail.com>
2021-03-02 10:46:34 +0700

    test(components): add test for setting currentProject to App component
    Jira issue: VEGA-3251

---
+++ __tests__/components/app/App.test.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { render } from '@testing-library/react';
+
+import { App } from '@/app/App';
+import { serviceConfig } from '@/helpers/sevice-config';
+
+jest.mock('@/app/AppView', () => {
+  return {
+    AppView: () => {
+      return <div />;
+    },
+  };
+});
+
+const mockProjectId = 'mock id';
+
+const renderApp = () => {
+  const mockGraphqlClient = new ApolloClient({ cache: new InMemoryCache() });
+  const mockIdentity = {
+    getToken: (): Promise<string> => new Promise((resolve) => setTimeout(() => resolve('token'))),
+  };
+
+  const mockCurrentProject = {
+    get: () => ({
+      vid: mockProjectId,
+    }),
+  };
+
+  return render(
+    <App
+      currentProject={mockCurrentProject}
+      graphqlClient={mockGraphqlClient}
+      identity={mockIdentity}
+    />,
+  );
+};
+
+describe('App', () => {
+  test('рендерится без ошибок', () => {
+    expect(renderApp).not.toThrow();
+  });
+
+  test('устанавливается project id', () => {
+    renderApp();
+
+    const { projectId } = serviceConfig;
+
+    expect(projectId).toBe(mockProjectId);
+  });
+});
